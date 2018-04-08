1d140a6abaed4765d9350ddb67613e370b932346
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-04-08 21:49:55 +0200

    Added pymongo to virtual envs

--- AnsibleTest/files/install_setup_tools_pip.sh
+++ AnsibleTest/files/install_setup_tools_pip.sh
@@ -9,6 +9,13 @@ wget https://bootstrap.pypa.io/2.6/get-pip.py -O get-pip2.6.py
 /usr/local/bin/pip3.5 install virtualenv pymongo;
 /usr/local/bin/virtualenv --python=/usr/local/bin/python2.6 py26env;
 /usr/local/bin/virtualenv --python=/usr/local/bin/python3.5 py35env;
-rm get-pip2.6.py
+chown vagrant:vagrant /home/vagrant/py26env;
+chown vagrant:vagrant /home/vagrant/py35env;
+rm get-pip2.6.py;
 rm get-pip.py;
+PS1='\s-\v\$';
+source /home/vagrant/py26env/bin/activate;
+pip install pymongo;
+source /home/vagrant/py35env/bin/activate;
+pip install pymongo;
 touch /home/vagrant/py_setuptools_pip.success;
