77d0caeaccfc4e5b630060f8c0c947f787f1a5a9
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-04-15 21:29:28 +0200

    Removed double braces

--- AnsibleTest/files/mongodb_replicaset.sh
+++ AnsibleTest/files/mongodb_replicaset.sh
@@ -6,9 +6,9 @@ START_PORT=$1;
 REPLSET=$2;
 
 cd /home/vagrant
-mkdir -p mongo_$(($((START_PORT + 1))));
-mkdir -p mongo_$(($((START_PORT + 2))));
-mkdir -p mongo_$(($((START_PORT + 3))));
+mkdir -p mongo_$((START_PORT + 1));
+mkdir -p mongo_$((START_PORT + 2));
+mkdir -p mongo_$((START_PORT + 3));
 
 mongod --shardsvr --smallfiles --nojournal --storageEngine wiredTiger --wiredTigerEngineConfigString="cache_size=200M" --dbpath mongo_$((START_PORT + 1)) --port $((START_PORT + 1)) --replSet "$REPLSET" --logpath mongo_$((START_PORT + 1))/log.log --fork;
 mongod --shardsvr --smallfiles --nojournal --storageEngine wiredTiger --wiredTigerEngineConfigString="cache_size=200M" --dbpath mongo_$((START_PORT + 2)) --port $((START_PORT + 2)) --replSet "$REPLSET" --logpath mongo_$((START_PORT + 2))/log.log --fork;
