d57125f8961fee58632b68f2d49ddb92aa2f22c7
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-04-23 22:57:17 +0200

    Reoganisation of files

--- AnsibleTest/files/mongodb_2shards.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e;
-set -u;
-
-/home/vagrant/bash/config_mongos_server.sh;
-
-/home/vagrant/bash/mongodb_replicaset.sh 3000 rs0;
-/home/vagrant/bash/mongodb_replicaset.sh 3003 rs1;
--- AnsibleTest/files/mongodb_3shards.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e;
-set -u;
-
-/home/vagrant/bash/config_mongos_server.sh;
-
-/home/vagrant/bash/mongodb_replicaset.sh 3000 rs0;
-/home/vagrant/bash/mongodb_replicaset.sh 3003 rs1;
-/home/vagrant/bash/mongodb_replicaset.sh 3006 rs2;
