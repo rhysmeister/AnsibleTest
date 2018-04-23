73300c7350769a9faa699934068b263c4d20a964
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-04-23 22:57:59 +0200

    2 shard playbooks

---
+++ AnsibleTest/files/bash/mongodb_replicaset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e;
+set -u;
+
+START_PORT=$1;
+REPLSET=$2;
+
+cd /home/vagrant
+mkdir -p mongo_$((START_PORT + 1));
+mkdir -p mongo_$((START_PORT + 2));
+mkdir -p mongo_$((START_PORT + 3));
+
+mongod --shardsvr --smallfiles --nojournal --storageEngine wiredTiger --wiredTigerEngineConfigString="cache_size=200M" --dbpath mongo_$((START_PORT + 1)) --port $((START_PORT + 1)) --replSet "$REPLSET" --logpath mongo_$((START_PORT + 1))/log.log --fork;
+mongod --shardsvr --smallfiles --nojournal --storageEngine wiredTiger --wiredTigerEngineConfigString="cache_size=200M" --dbpath mongo_$((START_PORT + 2)) --port $((START_PORT + 2)) --replSet "$REPLSET" --logpath mongo_$((START_PORT + 2))/log.log --fork;
+mongod --shardsvr --smallfiles --nojournal --storageEngine wiredTiger --wiredTigerEngineConfigString="cache_size=200M" --dbpath mongo_$((START_PORT + 3)) --port $((START_PORT + 3)) --replSet "$REPLSET" --logpath mongo_$((START_PORT + 3))/log.log --fork;
