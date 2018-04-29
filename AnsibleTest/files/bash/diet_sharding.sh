d1ee619d6dbb137fbdd8724b20dd999dbef97983
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-04-29 23:29:56 +0200

    Update diet sharding

--- AnsibleTest/files/bash/diet_sharding.sh
+++ AnsibleTest/files/bash/diet_sharding.sh
@@ -4,5 +4,8 @@ set -u;
 
 /home/vagrant/bash/config_mongos_server.sh;
 
+mkdir /home/vagrant/mongo_3001;
+mkdir /home/vagrant/mongo_3002;
+
 mongod --shardsvr --smallfiles --nojournal --storageEngine wiredTiger --wiredTigerEngineConfigString="cache_size=200M" --dbpath mongo_3001 --port 3001 --replSet rs0 --logpath mongo_3001/log.log --fork;
 mongod --shardsvr --smallfiles --nojournal --storageEngine wiredTiger --wiredTigerEngineConfigString="cache_size=200M" --dbpath mongo_3002 --port 3002 --replSet rs1 --logpath mongo_3002/log.log --fork;
