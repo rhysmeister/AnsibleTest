d2dee8ce3e9d4ee1d38c4200aa65d4523d03246d
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-04-29 14:19:21 +0200

    Replicaset config changes

---
+++ AnsibleTest/files/bash/diet_sharding.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e;
+set -u;
+
+/home/vagrant/bash/config_mongos_server.sh;
+
+mongod --shardsvr --smallfiles --nojournal --storageEngine wiredTiger --wiredTigerEngineConfigString="cache_size=200M" --dbpath mongo_3001 --port 3001 --replSet rs0 --logpath mongo_3001/log.log --fork;
+mongod --shardsvr --smallfiles --nojournal --storageEngine wiredTiger --wiredTigerEngineConfigString="cache_size=200M" --dbpath mongo_3002 --port 3002 --replSet rs1 --logpath mongo_3002/log.log --fork;
