091159c26db95690ed7a38bd6411794e663340bb
Rhys Campbell <rhys.james.campbell@googlemail.com>
2018-05-20 14:56:12 +0200

    A few tweak to env to get pep8 tests running

--- AnsibleTest/files/bash/install_setup_tools_pip.sh
+++ AnsibleTest/files/bash/install_setup_tools_pip.sh
@@ -5,8 +5,8 @@ wget https://bootstrap.pypa.io/get-pip.py
 wget https://bootstrap.pypa.io/2.6/get-pip.py -O get-pip2.6.py
 /usr/local/bin/python2.6 get-pip2.6.py;
 /usr/local/bin/python3.5 get-pip.py;
-/usr/local/bin/pip2.6 install virtualenv pymongo;
-/usr/local/bin/pip3.5 install virtualenv pymongo;
+/usr/local/bin/pip2.6 install virtualenv==15.2.0 pymongo;
+/usr/local/bin/pip3.5 install virtualenv==15.2.0 pymongo;
 /usr/local/bin/virtualenv --python=/usr/local/bin/python2.6 py26env;
 /usr/local/bin/virtualenv --python=/usr/local/bin/python3.5 py35env;
 chown vagrant:vagrant /home/vagrant/py26env;
